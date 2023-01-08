4248c2a5dc644b1613bfd1104232126f4729fa14
baekmir0505 <bmrbag0601@naver.com>
2023-01-08 09:48:35 +0900

    first commit

---
+++ day01_8.py
@@ -0,0 +1,19 @@
+# 대입연산자
+a = 0
+print(a)
+
+# 누적연산자
+a += 3     # a = a + 3 
+print(a)
+
+a += 2
+print(a)   # a = a + 2
+
+a + 2          # a값은 변경 없음   # a + 2
+print(a)
+
+a - 4
+print(a)      # a값은 변경 없음    # a - 4
+
+a -= 4      # a값이 변경됨      # a = a - 4
+print(a)
